85523fee6b681ff81982b897b6e27d0c67426870
Guðni Hrafn Guðlaugsson <gudnihrafn93@gmail.com>
2025-12-09 12:21:27 +0000

    favorites

--- components/movies/movies.tsx
+++ components/movies/movies.tsx
@@ -1,27 +1,64 @@
+import { addFavorite, removeFavorite } from "@/src/redux/favorites/favoritesSlice";
+import { RootState } from "@/src/redux/store";
+import { saveFavoritesToStorage } from "@/src/services/favoritesStorage";
 import { Movie as MovieType } from "@/src/types/types";
-import {View, Text, Image} from "react-native";
-import { styles } from "./styles"
+import { Ionicons } from '@expo/vector-icons';
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { styles } from "./styles";
 
 type movieProps = {
-    movie: MovieType
+  movie: MovieType;
 };
 
-export function Movie({movie} : movieProps){
-    return (
-         <View style={styles.container}>
-            {<Image
-            source={{uri: movie.poster}}
-            style={styles.poster}
-            resizeMode="cover"
-            />}
+export function Movie({ movie }: movieProps) {
+
+  const dispatch = useDispatch();
+  const favIds = useSelector((state: RootState) => state.favorites.ids);
+  const movieIdString = movie.id.toString();
+  const isFavorite = favIds.includes(movieIdString);
+
+  const toggleFavorite = async () => {
+    let updated;
+
+    if (isFavorite) {
+      updated = favIds.filter(id => id !== movieIdString);
+      dispatch(removeFavorite(movieIdString));
+    } else {
+      updated = [...favIds, movieIdString];
+      dispatch(addFavorite(movieIdString));
+    }
+
+    await saveFavoritesToStorage(updated);
+  };
+
+  return (
+    <View style={styles.container}>
+
+      <TouchableOpacity
+        style={{ position: "absolute", top: 8, right: 8, zIndex: 20 }}
+        onPress={toggleFavorite}
+      >
+        <Ionicons
+          name={isFavorite ? "star" : "star-outline"}
+          size={28}
+          color={isFavorite ? "#FFD700" : "#ccc"}
+        />
+      </TouchableOpacity>
+
+      <Image
+        source={{ uri: movie.poster }}
+        style={styles.poster}
+        resizeMode="cover"
+      />
+
       <View style={styles.info}>
         <Text style={styles.title}>{movie.title}</Text>
-        {
-        <Text style={styles.year}>{movie.year}</Text>}
-        {
+        <Text style={styles.year}>{movie.year}</Text>
+        <Text style={styles.year}>{movie.durationMinutes} min</Text>
         <Image source={{ uri: movie.certificateImg }} style={styles.certificate} />
-        }
       </View>
+
     </View>
-  )
+  );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import tokenReducer from "@/src/redux/features/token/token-slice";
 import moviesReducer from "@/src/redux/features/movies/movies-slice"
 import cinemasReducer from "@/src/redux/features/cinema/cinema-slice"
+import favoritesReducer from "@/src/redux/favorites/favoritesSlice"
 
 const store =  configureStore({
   reducer: {
     token: tokenReducer,
     movies: moviesReducer,
     cinemas: cinemasReducer,
+    favorites: favoritesReducer,
   },
 });
 
