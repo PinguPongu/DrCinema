d41971ff7521cf677321b8195d11810069347463
PeturDaniel <150845667+PeturDaniel@users.noreply.github.com>
2025-12-09 13:53:19 +0000

    cinema detail screen fallegur

--- components/cinema-details/cinema-details.tsx
+++ components/cinema-details/cinema-details.tsx
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import { styles } from "./styles";
 import { Cinema } from "../cinema/cinema";
 import { useMovies } from "@/hooks/data";
+import { IconSymbol } from '@/components/ui/icon-symbol';
 
 type CinemaProps ={
   cinema: CinemaType;
@@ -11,16 +12,39 @@ type CinemaProps ={
 
 export default function CinemaDetailsItem({ cinema } : CinemaProps) {
   const movies = useMovies();
+  const regex = /(<([^>]+)>)/gi;
+  let cinemaDescription = "";
+  if (cinema.description.length !== 0) {
+    cinemaDescription = cinema.description.replace(regex, "");
+  } else {
+    cinemaDescription = cinema.description;
+  };
   
   console.log(cinema);
   return (
     <View style={styles.topContainer}>
-      <Text>{cinema.name}</Text>
-      <Text>{cinema.description}</Text>
-      <Text>{cinema.address}</Text>
-      <Text>{cinema.city}</Text>
-      <Text>{cinema.phone}</Text>
-      <Text>{cinema.website}</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{cinema.name}</Text>
+      </View>
+      {cinemaDescription.length !== 0 && (
+        <View>
+          <Text style={styles.descriptionHeading}>Um {cinema.name}:</Text>
+          <Text style={styles.description}>{cinemaDescription}</Text>
+        </View>
+      )}
+      <View style={styles.smallContainer}>
+        <IconSymbol name="globe" color={"black"}></IconSymbol>
+        <Text style={styles.extra}>{cinema.address}, </Text>
+        <Text>{cinema.city}</Text>
+      </View>
+      <View style={styles.smallContainer}>
+        <IconSymbol name="phone" color={"black"}></IconSymbol>
+        <Text style={styles.extra}>{cinema.phone}</Text>
+      </View>
+      <View style={styles.smallContainer}>
+        <IconSymbol name="link" color={"black"}></IconSymbol>
+        <Text style={styles.extra}>{cinema.website}</Text>
+      </View>
       <Cinema cinema={cinema} movies={movies}/>
 
     </View>
--- components/cinema-details/styles.ts
+++ components/cinema-details/styles.ts
@@ -5,4 +5,32 @@ export const styles = StyleSheet.create({
   topContainer: {
     backgroundColor: "white",
   },
+  titleContainer: {
+    alignItems: "center",
+  },
+  title: {
+    marginTop: 4,
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  descriptionHeading: {
+    marginLeft: 3,
+    fontSize: 18,
+  },
+  description: {
+    marginTop: 5,
+    marginLeft: 4,
+    marginRight: 4,
+    marginBottom: 4,
+  },
+  smallContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 5,
+    marginBottom: 3,
+  },
+  extra: {
+    marginLeft: 4,
+  },
+
 });
