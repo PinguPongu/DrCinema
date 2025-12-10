8a45a6f543f1cb451527d253bd3e70dc295efdc7
PeturDaniel <150845667+PeturDaniel@users.noreply.github.com>
2025-12-10 11:09:52 +0000

    upcoming movies sorted by release date

--- src/views/upcoming/upcoming.tsx
+++ src/views/upcoming/upcoming.tsx
@@ -7,11 +7,15 @@ import { styles } from "./styles";
 
 export default function Upcoming() {
   const upcomingMovies = useUpcomingMovies();
+  const sortedMovies = [...upcomingMovies].sort((a, b) =>
+    a.year.localeCompare(b.year)
+  );
+  
   console.log(upcomingMovies);
   return (
     <ScrollView>
         <SafeAreaView style={styles.topContainer}>
-            {upcomingMovies.map((movie) => (
+            {sortedMovies.map((movie) => (
               <Movie key={movie.id} movie={movie}/>
             ))}         
         </SafeAreaView>
