bdd5c7d6f39e28968a40856732436836fb89d8ed
Guðni Hrafn Guðlaugsson <gudnihrafn93@gmail.com>
2025-12-10 10:52:26 +0000

    all changes

--- src/views/upcoming/upcoming.tsx
+++ src/views/upcoming/upcoming.tsx
@@ -1,19 +1,19 @@
+import { Movie } from "@/components/movies/movies";
 import { useUpcomingMovies } from "@/hooks/data";
 import { ScrollView } from "react-native";
-import { Movie } from "@/components/movies/movies";
-import { styles } from "./styles";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "./styles";
 
 
 export default function Upcoming() {
   const upcomingMovies = useUpcomingMovies();
-
+  console.log(upcomingMovies);
   return (
     <ScrollView>
         <SafeAreaView style={styles.topContainer}>
             {upcomingMovies.map((movie) => (
               <Movie key={movie.id} movie={movie}/>
-            ))}
+            ))}         
         </SafeAreaView>
     </ScrollView>
   );
