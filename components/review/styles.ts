25eb371e6080358cba53115093ec972699807a64
PeturDaniel <150845667+PeturDaniel@users.noreply.github.com>
2025-12-11 11:17:33 +0000

    review er ordid mjog fallegt

---
+++ components/review/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  title: {
+    fontWeight: "bold",
+  },
+  starContainer: {
+    flexDirection: "row",
+  },
+  reviewField: {
+    borderRadius: 10,
+    borderWidth: 1,
+    height: 100,
+    paddingHorizontal: 5,
+  },
+})
